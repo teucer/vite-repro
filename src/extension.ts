08e7fbf9a86beaf2f77eb81ece46df8e71094686
teucer <pehlewan@gmail.com>
2022-06-07 02:09:41 +0200

    yaml 3

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,76 @@
 import { tags as t } from "@lezer/highlight";
-import { MarkdownConfig, LeafBlockParser, BlockContext, LeafBlock } from "@lezer/markdown";
+import { MarkdownConfig, LeafBlockParser, BlockContext, LeafBlock, Line } from "@lezer/markdown";
 
 const regex =
   /^\s*(?:[,[\]{}&*!|>'"%@`][^\s'":]|[^,[\]{}#&*!|>'"%@`])[^#]*?(?=\s*:($|\s((?:>-?|\|-?))?))/;
+
+export enum Type {
+  Yaml = "Yaml",
+  YamlKey = "YamlKey",
+  YamlItem = "YamlItem"
+}
+
+function isSpace(ch: number) {
+  return ch === 32 || ch === 9 || ch === 10 || ch === 13;
+}
+
+function inYaml(cx: BlockContext, type: Type) {
+  for (let i = cx.depth - 1; i >= 0; i--) {
+    if (cx.parentType(i).name === type) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isYaml(cx: BlockContext, line: Line, isRoot: boolean) {
+  const pos = line.pos;
+  // const next = line.next;
+  // skip the the number/key -> pos is at after the number
+
+  if (
+    // pos === line.pos || // we did not skip anything
+    // pos > line.pos + 9 || // too many spaces before the number
+    // (next !== 46 && next !== 41) /* '.)' */ || // it is not the separator/operator
+    // // there is no space between number and the rest
+    (pos < line.text.length - 1 && !isSpace(line.text.charCodeAt(pos + 1))) ||
+    // ok now we have [Number. XXX] somewhere in the line. It is not yaml IF
+    // // it is not root AND
+    // // it is not in a yaml block AND
+    (!isRoot &&
+      !inYaml(cx, Type.Yaml) &&
+      (line.skipSpace(pos + 1) === line.text.length || // the rest is only spaces
+        pos > line.pos + 1 || // we are after the second character
+        line.next !== 49)) /* '1' */ // the start is not 1
+  ) {
+    return -1;
+  }
+  return pos + 1 - line.pos; // size of the number
+}
+
+function getYamlIndent(line: Line, pos: number) {
+  const indentAfter = line.countIndent(pos, line.pos, line.indent);
+  const indented = line.countIndent(line.skipSpace(pos), pos, indentAfter);
+  return indented >= indentAfter + 5 ? indentAfter + 1 : indented;
+}
+
+function YamlParser(cx: BlockContext, line: Line) {
+  const size = isYaml(cx, line, true); // do we have a valid [1. XXX] in the line?
+
+  if (size < 0) {
+    return false;
+  }
+
+  if (cx.block.type !== Type.Yaml) {
+    cx.startComposite(Type.Yaml, line.basePos, line.text.charCodeAt(line.pos + size - 1));
+  }
+
+  const newBase = getYamlIndent(line, line.pos + size);
+  cx.startComposite(Type.YamlItem, line.basePos, newBase - line.baseIndent);
+  cx.addElement(cx.elt(Type.YamlKey, cx.lineStart + line.pos, cx.lineStart + line.pos + size));
+  line.moveBaseColumn(newBase);
+  return null;
+}
 
 class YamlKeyParser implements LeafBlockParser {
   match: RegExpExecArray;
@@ -48,7 +116,10 @@ export const YamlKey: MarkdownConfig = {
   parseBlock: [
     {
       name: "YamlParser",
-      endLeaf: () => {
+      endLeaf: (cx: BlockContext, line: Line) => {
+        if (isSpace(line.pos)) {
+          return false;
+        }
         return true;
       },
       leaf(_, leaf) {
